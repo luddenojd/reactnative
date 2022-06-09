6b26d6617443998e87009198108b47804cfe81f9
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-06-09 14:46:29 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Text");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "white" }}>
+        Open up App.js to start working on your app!
+      </Text>
+
+      <Text style={{ color: "white" }}>Hello world!</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => setName(text)}
+        onSubmitEditing={() => {
+          alert(name);
+        }}
+        value={name}
+      />
+      <Button
+        onPress={() => {
+          alert();
+        }}
+        title="Hämta städer"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    backgroundColor: "white",
+    borderWidth: 1,
   },
 });
